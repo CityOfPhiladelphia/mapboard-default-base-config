71907bd9fca750bf481163f509865cf76a31b544
Andy Rothwell <rothwell.andy@gmail.com>
2020-08-06 12:37:32 -0400

    removes indentation to fix api call

--- config.js
+++ config.js
@@ -293,14 +293,14 @@
             },
             source: {
               tiles: [ '\
-        https://tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/PWDParcel_ImageryOverlay/MapServer/export?dpi=110\
-        &transparent=true\
-        &format=png36\
-        &bbox={bbox-epsg-3857}\
-        &bboxSR=3857\
-        &imageSR=3857\
-        &size=512,512\
-        &f=image\
+https://tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/PWDParcel_ImageryOverlay/MapServer/export?dpi=110\
+&transparent=true\
+&format=png36\
+&bbox={bbox-epsg-3857}\
+&bboxSR=3857\
+&imageSR=3857\
+&size=512,512\
+&f=image\
             ' ],
               // type: 'raster',
               // tileSize: 256,
